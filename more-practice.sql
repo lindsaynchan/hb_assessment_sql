a2be50e3f515fc3c38be019b83831bb1914f13de
lindsaynchan <lindsaynchan@gmail.com>
2016-07-30 14:38:35 -0700

    First commit more-practice.sql

---
+++ more-practice.sql
@@ -0,0 +1,23 @@
+-- Include your solutions to the More Practice problems in this file.
+
+
+
+-- INSERT
+
+INSERT INTO models (year, brand_name, name) VALUES (2015,'Chevrolet','Malibu');
+INSERT INTO models (year, brand_name, name) VALUES (2015,'Subaru','Outback');
+
+
+-- CREATE TABLE
+
+CREATE TABLE awards(
+    name VARCHAR(20),
+    year INTEGER,
+    winner_id INTEGER
+    );
+
+-- More INSERT
+
+INSERT INTO models (name, year, winner_id) VALUES ('IIHS Safety Award',2015,49);
+INSERT INTO models (name, year, winner_id) VALUES ('IIHS Safety Award',2015,50);
+
